175b7477a45c54901289fc072095ff50fe723bc3
Akhil G Krishnan <akhilgkrishnan102@gmail.com>
2026-05-17 14:19:33 +0530

    Added upload API

--- script.js
+++ script.js
@@ -7,32 +7,10 @@ alert("Select PDF");
 return;
 }
 
-const uploadData = {
-
-type:document.getElementById("type").value,
-className:document.getElementById("class").value,
-stream:document.getElementById("stream").value,
-university:document.getElementById("university").value,
-course:document.getElementById("course").value,
-semester:document.getElementById("semester").value,
-year:document.getElementById("year").value
-
-};
-
-let formData=new FormData();
-
-formData.append("pdf",file);
-
-Object.keys(uploadData).forEach(key=>{
-
-formData.append(key,uploadData[key]);
-
-});
-
 let response=await fetch("/api/upload",{
 
 method:"POST",
-body:formData
+body:file
 
 });
 
@@ -64,60 +42,6 @@ papers.innerHTML += `
 
 <div class="paper">
 
-<h3>${file.name}</h3>
-
-<a href="${file.download_url}" target="_blank">
-<button>View</button>
-</a>
-
-<a href="${file.download_url}" download>
-<button>Download</button>
-</a>
-
-</div>
-
-`;
-
-});
-
-}catch(error){
-
-console.log(error);
-
-}
-
-}
-
-
-
-
-
-
-
-
-
-
-
-async function loadPapers(){
-
-try{
-
-const response=await fetch(
-"https://api.github.com/repos/akhilgk102/question-paper-hub/contents/pdf"
-);
-
-const files=await response.json();
-
-let papers=document.getElementById("papers");
-
-papers.innerHTML="";
-
-files.forEach(file=>{
-
-papers.innerHTML += `
-
-<div class="paper">
-
 <h3>📄 ${file.name}</h3>
 
 <div class="btns">
@@ -125,17 +49,13 @@ papers.innerHTML += `
 <a href="https://docs.google.com/gview?embedded=true&url=${encodeURIComponent(file.download_url)}"
 target="_blank">
 
-<button>
-View
-</button>
+<button>View</button>
 
 </a>
 
 <a href="${file.download_url}" download>
 
-<button>
-Download
-</button>
+<button>Download</button>
 
 </a>
 
