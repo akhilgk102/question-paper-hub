435c67bf1135d983f615270b71f26bd337aed757
Akhil G Krishnan <akhilgkrishnan102@gmail.com>
2026-05-17 13:48:48 +0530

    Added upload API

--- api/upload.js
+++ api/upload.js
@@ -1,18 +1,10 @@
-export default async function handler(req,res){
+export default function handler(req,res){
 
-const token=process.env.GITHUB_TOKEN;
+return res.status(200).json({
 
-const response=await fetch(
-"https://api.github.com/user",
-{
-headers:{
-Authorization:`token ${token}`
-}
-}
-);
+hasToken: !!process.env.GITHUB_TOKEN,
+tokenStart: process.env.GITHUB_TOKEN?.substring(0,15)
 
-const data=await response.json();
-
-res.status(200).json(data);
+});
 
 }
