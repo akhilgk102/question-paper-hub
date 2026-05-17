1eceb15484e1b6b9d216e172aa8d859dea23f38c
Akhil G Krishnan <akhilgkrishnan102@gmail.com>
2026-05-17 14:11:35 +0530

    Added upload API

--- api/upload.js
+++ api/upload.js
@@ -26,7 +26,8 @@ export default async function handler(req, res) {
 
     const buffer = await getBuffer(req);
 
-    const filename=`paper_${Date.now()}.pdf`;
+    const filename=
+    `${type}_${course}_${semester}_${year}_${Date.now()}.pdf`;
 
     const content=buffer.toString("base64");
 
