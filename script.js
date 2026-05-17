0762dfd266024a67a26409b06388a531972a506a
Akhil G Krishnan <akhilgkrishnan102@gmail.com>
2026-05-17 15:03:44 +0530

    Dynamic QuesVault

--- script.js
+++ script.js
@@ -48,76 +48,71 @@ document.getElementById(
 }
 
 
+
+
+
 async function loadPapers(){
 
 try{
 
-const response=
-await fetch(
+const response=await fetch(
 "https://api.github.com/repos/akhilgk102/question-paper-hub/contents/pdf"
 );
 
-const files=
-await response.json();
+const files=await response.json();
+
+const category=
+document.getElementById("category").value.toLowerCase();
+
+const university=
+document.getElementById("university").value.toLowerCase();
+
+const course=
+document.getElementById("course").value.toLowerCase();
+
+const semester=
+document.getElementById("semester").value.toLowerCase();
+
+const year=
+document.getElementById("year").value.toLowerCase();
 
 let papers=
-document.getElementById(
-"papers"
-);
+document.getElementById("papers");
 
 papers.innerHTML="";
-
-let university=document.getElementById("university").value;
-let course=document.getElementById("course").value;
-let semester=document.getElementById("semester").value;
 
 files.forEach(file=>{
 
 let name=file.name.toLowerCase();
 
 if(
-(university && !name.includes(university.toLowerCase()))
-||
-(course && !name.includes(course.toLowerCase()))
-||
-(semester && !name.includes(semester.toLowerCase()))
+(category && !name.includes(category)) ||
+(university && !name.includes(university.replaceAll(" ","_"))) ||
+(course && !name.includes(course.replaceAll(" ","_"))) ||
+(semester && !name.includes(semester.replaceAll(" ","_"))) ||
+(year && !name.includes(year))
 ){
 return;
 }
 
-papers.innerHTML +=`
+papers.innerHTML += `
 
 <div class="paper">
 
-<h3>
-
-📄 ${file.name}
-
-</h3>
+<h3>📄 ${file.name}</h3>
 
 <div class="btns">
 
-<a
-href="https://docs.google.com/gview?embedded=true&url=${encodeURIComponent(file.download_url)}"
+<a href="https://docs.google.com/gview?embedded=true&url=${encodeURIComponent(file.download_url)}"
 target="_blank">
 
-<button>
-
-View
-
-</button>
+<button>View</button>
 
 </a>
 
-<a
-href="${file.download_url}"
-download>
+<a href="${file.download_url}" download>
 
-<button>
-
-Download
-
-</button>
+<button>Download</button>
 
 </a>
 
@@ -129,12 +124,9 @@ Download
 
 });
 
-}
-catch(error){
+}catch(error){
 
-console.log(
-error
-);
+console.log(error);
 
 }
 
