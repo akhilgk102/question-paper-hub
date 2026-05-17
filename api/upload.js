56643e2fbf7fba1b1a1568afd7322b325e252a55
Akhil G Krishnan <akhilgkrishnan102@gmail.com>
2026-05-17 13:39:28 +0530

    Added upload API

--- api/upload.js
+++ api/upload.js
@@ -1,60 +1,18 @@
-export const config = {
-  api: {
-    bodyParser: false
-  }
-};
+export default async function handler(req,res){
 
-async function getBuffer(req) {
-  const chunks = [];
+const token=process.env.GITHUB_TOKEN;
 
-  for await (const chunk of req) {
-    chunks.push(chunk);
-  }
-
-  return Buffer.concat(chunks);
+const response=await fetch(
+"https://api.github.com/user",
+{
+headers:{
+Authorization:`token ${token}`
 }
+}
+);
 
-export default async function handler(req, res) {
+const data=await response.json();
 
-  try {
-
-    const buffer = await getBuffer(req);
-
-    const filename = `paper_${Date.now()}.pdf`;
-
-    const token = process.env.GITHUB_TOKEN;
-    const username = process.env.GITHUB_USERNAME;
-    const repo = process.env.GITHUB_REPO;
-
-    const content = buffer.toString("base64");
-
-    const githubResponse = await fetch(
-      `https://api.github.com/repos/${username}/${repo}/contents/pdf/${filename}`,
-      {
-        method: "PUT",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          Accept: "application/vnd.github+json",
-          "Content-Type": "application/json"
-        },
-
-        body: JSON.stringify({
-          message: `Upload ${filename}`,
-          content: content
-        })
-      }
-    );
-
-    const data = await githubResponse.json();
-
-    return res.status(githubResponse.status).json(data);
-
-  } catch(error){
-
-    return res.status(500).json({
-      message:error.message
-    });
-
-  }
+res.status(200).json(data);
 
 }
